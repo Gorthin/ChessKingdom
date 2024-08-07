eb89d0576b904910d875871fe272ef22153c593f
Adrian Szymanski <adrian.szymanski@nokia.com>
2024-08-07 07:52:31 +0200

    Add settings file and initializing game window

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """The class is designed to store all game settings."""
+
+    def __init__(self):
+        """Initializing the game settings."""
+        #Setting of screen
+        self.screen_width = 800
+        self.screen_height = 640
+        self.bg_color = (0, 50, 0)
