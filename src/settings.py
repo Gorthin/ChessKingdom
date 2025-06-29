ab076185e984d5b2127136c13bd7daf98d91c82c
Gorthin <adrian88szymanski@gmail.com>
2025-06-29 16:08:22 +0200

    Changed background colour

--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,6 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 640
         self.square_size = 50
-        self.bg_color = (0, 50, 0)
+        self.bg_color = (0, 150, 0)
         self.white_color = (232, 235, 239)
         self.black_color = (125, 135, 150)
